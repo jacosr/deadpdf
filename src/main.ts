2b567b78a18b55e350110718ede3825e025cb4b7
twakj <twakjaco@hotmail.com>
2026-01-31 16:02:17 -0500

    load data via dpdf://

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,33 @@
-//import * as domain from 'domain';
 import { app, BrowserWindow, protocol } from 'electron';
 import * as path from 'path';
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-
-  win.loadFile(path.join(__dirname, 'renderer/index.html'));
-}
 app.whenReady().then(() => {
+
+    protocol.registerBufferProtocol('dpdf', (request, callback) => {
+        const url = request.url.replace('dpdf://', '');    // all urls start with dpdf://
+        
+        if (url === 'index.html') {
+            const html = `
+                <!doctype html>
+                <html>
+                    <body>
+                        <h1>Loaded via dpdf:// protocol</h1>
+                        <p>content from custom protocol handler</p>
+                    </body>
+                </html>
+            `;
+
+            callback({
+                mimeType: 'text/html',
+                data: Buffer.from(html)
+            });
+            return;
+        }
+
+        // default fallback
+        callback({data: Buffer.from('Not found'), mimeType: 'text/plain' });
+    });
+
     createWindow();
 
     app.on('activate', () => {
@@ -29,3 +43,14 @@ app.on('window-all-closed', () => {
     }
 });
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadURL('dpdf://index.html');
+}
